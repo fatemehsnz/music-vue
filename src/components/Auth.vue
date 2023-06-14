e0c5b5d95c4316f3fe501e488f9c17f8fa08bb1b
fatemeh hasanzadeh <fatemeh.hasanzadeh97@yahoo.com>
2023-06-14 16:10:54 +0330

    name validation

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -73,15 +73,18 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form v-show="tab === 'register'">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <input
+              <vee-field
+                name="name"
                 type="text"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <!-- :rules="'required'" -->
+              <ErrorMessage class="text-red-600" name="name"/>
             </div>
             <!-- Email -->
             <div class="mb-3">
@@ -140,7 +143,7 @@
             >
               Submit
             </button>
-          </form>
+          </vee-form>
         </div>
       </div>
     </div>
@@ -156,6 +159,15 @@ export default {
   data() {
     return{
       tab : "login",
+      schema : {
+        name : "required",
+        email : "",
+        age : "",
+        password : "",
+        confirm_password : "",
+        country : "",
+        tos : "",
+      }
     }
   },
   computed : {
