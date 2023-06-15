1d3b95298f06c8d9c6ab9a083d4130ec1a115c57
fatemeh hasanzadeh <fatemeh.hasanzadeh97@yahoo.com>
2023-06-15 18:29:38 +0330

    custom links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,19 +3,22 @@
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
       <!-- <a href="#">Music</a> -->
-      <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/about">Music</router-link>
+      <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/">Music</router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
           <li>
+            <router-link class="px-2 text-white" to="/about">About</router-link>
+          </li>
+          <li>
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
               >Login / Register</a
             >
           </li>
           <li>
-            <a class="px-2 text-white" href="#">Manage</a>
+            <router-link class="px-2 text-white" to="/manage">Manage</router-link>
           </li>
         </ul>
       </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Manage from '../views/Manage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +17,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/About.vue')
-    }
+    },
+    {
+      path: '/manage',
+      name: 'manage',
+      component: Manage
+    },
   ]
 })
 
